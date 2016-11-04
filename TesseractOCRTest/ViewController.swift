3629062f04cd5a06c64dc5834afeedb30a77cb9d
Developer-MINO <mino.kim.1st@gmail.com>
2016-11-05 03:34:46 +0900

    Tesseract

--- TesseractOCRTest/ViewController.swift
+++ TesseractOCRTest/ViewController.swift
@@ -7,14 +7,28 @@
 //
 
 import UIKit
+import TesseractOCR
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, G8TesseractDelegate {
+    @IBOutlet weak var textView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let tesseract = G8Tesseract(language: "eng+kor") {
+            tesseract.delegate = self
+            //tesseract.charWhitelist = "0123456789"
+            tesseract.image = UIImage(named: "screen_eng")?.g8_blackAndWhite()
+            tesseract.recognize()
+            
+            textView.text = tesseract.recognizedText
+        }
     }
 
+    func progressImageRecognition(for tesseract: G8Tesseract!) {
+        print("Recognition Progress \(tesseract.progress)%")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
